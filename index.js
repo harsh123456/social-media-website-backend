b0b5a3dbdf1e82c992468decf1daff6c64df2ae3
Harsh Kansara <harshkansara98@gmail.com>
2021-06-08 13:51:03 +1200

    Made changes to the index.js file

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.get('/', (req, res) => {
   res.send('Hello to Memories API');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+//const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
