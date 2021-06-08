0a30dda08f552d532b4ed2cfdde4a8c713bc7150
Harsh Kansara <harshkansara98@gmail.com>
2021-06-08 12:35:29 +1200

    Created the server side of social media post where in user can like a comment, delete a comment and edit a comment

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,9 +1,11 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
 import PostMessage from '../models/postMessage.js';
 
-export const getPosts = async (req, res) => {
+export const getPosts = async (req, res) => { 
     try {
-        const postMessages = await PostMessage.find();
-        console.log(postMessages);
+        const postMessages = await PostMessage.find();         
         res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message });
@@ -21,4 +23,38 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id))
+        return res.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send('No post with that id');
+    
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post Deleted Successfully' });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id))
+        return res.status(404).send('No post with that id');
+    
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true });
+
+    res.json(updatedPost);
 }
